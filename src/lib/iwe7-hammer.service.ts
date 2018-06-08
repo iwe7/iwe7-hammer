8f9edf6d764f40a4f73d7d7f55bd04fd980a6bca
iwe7 <admin@meepo.com.cn>
2018-06-08 14:04:11 +0800

    first commit

---
+++ src/lib/iwe7-hammer.service.ts
@@ -0,0 +1,87 @@
+import { HammerGestureConfig, HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
+import { InjectionToken, Optional, Inject, Provider, NgModule } from '@angular/core';
+
+export interface HammerInstance {
+  on(eventName: string, callback: Function): void;
+  off(eventName: string, callback: Function): void;
+}
+export const HAMMER_OPTIONS = new InjectionToken<HammerOptions>('HAMMER_OPTIONS');
+// 默认全开启
+export class Iwe7HammerGestureConfig extends HammerGestureConfig {
+  private _hammer: HammerStatic = typeof window !== 'undefined' ? (window as any).Hammer : null;
+  // 原生
+  events: string[] = [
+    'longpress',
+    'slide',
+    'slidestart',
+    'slideend',
+    'slideright',
+    'slideleft',
+  ];
+  constructor(
+    @Optional()
+    @Inject(HAMMER_OPTIONS)
+    public _hammerOptions: HammerOptions
+  ) {
+    super();
+  }
+  buildHammer(element: HTMLElement): HammerInstance {
+    console.log(this._hammerOptions);
+    const mc = new this._hammer(element, this._hammerOptions || undefined);
+    const pan = new this._hammer.Pan();
+    const swipe = new this._hammer.Swipe();
+    const press = new this._hammer.Press();
+    const slide = this._createRecognizer(pan, { event: 'slide', threshold: 0 }, swipe);
+    const longpress = this._createRecognizer(press, { event: 'longpress', time: 500 });
+    pan.recognizeWith(swipe);
+    // 添加 swiper press pan slide longpress
+    mc.add([swipe, press, pan, slide, longpress]);
+    return mc as HammerInstance;
+  }
+
+  private _createRecognizer(base: Recognizer, options: any, ...inheritances: Recognizer[]): Recognizer {
+    let recognizer = new (base.constructor as RecognizerStatic)(options);
+    inheritances.push(base);
+    // 同时识别
+    inheritances.forEach(item => recognizer.recognizeWith(item));
+    return recognizer;
+  }
+}
+
+const hammerOptions: HammerOptions = {
+  cssProps: {
+    touchCallout: 'none',
+    touchSelect: 'none',
+    userDrag: 'none',
+    userSelect: 'none',
+    tapHighlightColor: 'red',
+    contentZooming: 'none'
+  }
+};
+
+export const Iwe7HammerOptionsProvider: Provider = [
+  {
+    provide: HAMMER_OPTIONS,
+    useValue: hammerOptions
+  }
+];
+
+export const Iwe7HammerConfigProvider: Provider = [
+  {
+    provide: HAMMER_GESTURE_CONFIG,
+    useClass: Iwe7HammerGestureConfig,
+    deps: [HAMMER_OPTIONS]
+  }
+];
+
+export const Iwe7HammerProvider: Provider = [
+  Iwe7HammerOptionsProvider,
+  Iwe7HammerConfigProvider
+];
+
+@NgModule({
+  providers: [Iwe7HammerProvider]
+})
+export class Iwe7HammerModule {
+
+}
